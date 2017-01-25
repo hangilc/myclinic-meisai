09c619e333422e10cb35853a65d76ec7ea59d92e
hangilc <hangil@chang.jp>
2017-01-25 23:02:59 +0900

    added connect-slashes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 var path = require("path");
+var slashes = require("connect-slashes");
 
 exports.staticDir = path.join(__dirname, "static");
 
@@ -7,14 +8,22 @@ function initApp(app, config) {
 	app.set("view engine", "ejs");
 	app.set("views", __dirname + "/views");
 	app.get("/", function(req, res){
-		res.render("index", { params: req.query });
-	});
-	
-	app.get("/preview", function(req, res){
+		var visitId = req.query.visit_id;
+		console.log(visitId);
+		if( visitId === undefined || visitId === "" ){
+			res.render("error", { message: "visit_id が指定されていません。"});
+			return;
+		}
 		res.render("preview", {
 			data: {x:10, y:20}
-		});
+		})
 	});
+	
+	// app.get("/preview", function(req, res){
+	// 	res.render("preview", {
+	// 		data: {x:10, y:20}
+	// 	});
+	// });
 	
     app.get("/config", function (req, res) {
         res.set({
@@ -22,5 +31,7 @@ function initApp(app, config) {
         });
         res.send("var config = " + JSON.stringify(config) + ";");
     });
+
+    app.use(slashes());
 }
 exports.initApp = initApp;
