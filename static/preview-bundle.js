d99a66c5ceb31998669fe93210dba98ff2087256
Hangil Chang <hangil@chang.jp>
2017-01-27 16:26:56 +0900

    render frames of lower box

--- static/preview-bundle.js
+++ static/preview-bundle.js
@@ -11638,7 +11638,7 @@
 	        comp.setFont("title-font");
 	        comp.textAt("診療明細書", box.cx(), 30, "center", "center");
 	        this.renderUpperBox(upperBox);
-	        comp.box(lowerBox);
+	        this.renderLowerBox(lowerBox);
 	    }
 	    done() {
 	        let ops = this.comp.getOps();
@@ -11671,6 +11671,19 @@
 	        cols.forEach(col => {
 	            comp.box(col);
 	        });
+	        comp.setFont("regular");
+	        {
+	            let c = cols[0];
+	            comp.textIn("患者番号", c, "center", "center");
+	        }
+	        {
+	            let c = cols[2];
+	            comp.textIn("氏名", c, "center", "center");
+	        }
+	        {
+	            let c = cols[4];
+	            comp.textIn("受診日", c, "center", "center");
+	        }
 	    }
 	    renderUpperBoxRow2(box) {
 	        let comp = this.comp;
@@ -11679,6 +11692,33 @@
 	        cols.forEach(col => {
 	            comp.box(col);
 	        });
+	        comp.setFont("regular");
+	        {
+	            let c = cols[0];
+	            comp.textIn("受診科", c, "center", "center");
+	        }
+	    }
+	    renderLowerBox(box) {
+	        let comp = this.comp;
+	        comp.box(box);
+	        let rows = box.splitToRows(5);
+	        this.renderLowerBoxRow1(rows[0]);
+	        this.renderLowerBoxRow2(rows[1]);
+	    }
+	    renderLowerBoxRow1(box) {
+	        let comp = this.comp;
+	        let cols = box.splitToColumnsByWidths(16, 81, 22.5);
+	        cols.forEach(col => comp.box(col));
+	        comp.setFont("regular");
+	        comp.textIn("部", cols[0], "center", "center");
+	        comp.textIn("項　目　名", cols[1], "center", "center");
+	        comp.textIn("点　数", cols[2], "center", "center");
+	        comp.textIn("回　数", cols[3], "center", "center");
+	    }
+	    renderLowerBoxRow2(box) {
+	        let comp = this.comp;
+	        let cols = box.splitToColumnsByWidths(16, 81, 22.5);
+	        cols.forEach(col => comp.box(col));
 	    }
 	}
 	exports.MeisaiForm = MeisaiForm;
