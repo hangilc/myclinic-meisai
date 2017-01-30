4e6e357d5d565037e056bd1d4588e9d65859d355
hangilc <hangil@chang.jp>
2017-01-31 00:02:20 +0900

    render upper box with layout

--- browser-src/meisai-form.ts
+++ browser-src/meisai-form.ts
@@ -6,12 +6,147 @@ class FontSpec {
 	constructor(public name: string, public fontName: string, public size: number){};
 }
 
+class Point {
+	x: number;
+	y: number;
+	constructor(x: number, y: number){
+		this.x = x;
+		this.y = y;
+	}
+}
+
+class RenderEnv {
+	titleFont: string;
+	regularFont: string;
+}
+
+class Layout {
+	titleCenterPoint: Point;
+	upperBox: UpperBoxLayout;
+	lowerBox: LowerBoxLayout;
+
+	constructor(){
+		let page = Box.createA4Box();
+		let box = page.innerBox(30, 42, 30 + 140, 42 + 10 + 4 + 185);
+		let [upperBox, _, lowerBox] = box.splitToRows(10, 14);
+		this.titleCenterPoint = new Point(box.cx(), 30);
+		this.upperBox = new UpperBoxLayout(upperBox);
+		this.lowerBox = new LowerBoxLayout(lowerBox);
+	}
+
+	renderBlank(comp: Compiler, env: RenderEnv): void {
+		comp.setFont(env.titleFont);
+		comp.textAt("診療明細書", this.titleCenterPoint.x, this.titleCenterPoint.y, "center", "center");
+		comp.setFont(env.regularFont);
+		this.upperBox.renderBlank(comp, env);
+		this.lowerBox.renderBlank(comp, env);
+	}
+}
+
+class UpperBoxLayout {
+	frame: Box;
+	row1: UpperBoxRow1Layout;
+	row2: UpperBoxRow2Layout;
+
+	constructor(box: Box){
+		this.frame = box;
+		let [row1, row2] = box.splitToEvenRows(2);
+		this.row1 = new UpperBoxRow1Layout(row1);
+		this.row2 = new UpperBoxRow2Layout(row2);
+	}
+
+	renderBlank(comp: Compiler, env: RenderEnv): void {
+		comp.box(this.frame);
+		comp.frameBottom(this.row1.frame);
+		this.row1.renderBlank(comp, env);
+		this.row2.renderBlank(comp, env);
+	}
+}
+
+class UpperBoxRow1Layout {
+	frame: Box;
+	cols: Box[];
+
+	constructor(box: Box){
+		this.frame = box;
+		this.cols = box.splitToColumnsByWidths(17, 30, 10, 40, 16);
+	}
+
+	renderBlank(comp: Compiler, env: RenderEnv): void {
+		let cols = this.cols;
+		for(let i=0;i<cols.length-1;i++){
+			comp.frameRight(cols[i]);
+		}
+		comp.textIn("患者番号", cols[0], "center", "center");
+		comp.textIn("氏名", cols[2], "center", "center");
+		comp.textIn("受診日", cols[4], "center", "center");
+	}
+}
+
+class UpperBoxRow2Layout {
+	frame: Box;
+	cols: Box[];
+
+	constructor(box: Box){
+		this.frame = box;
+		this.cols = box.splitToColumnsByWidths(17);
+	}
+
+	renderBlank(comp: Compiler, env: RenderEnv): void {
+		comp.frameRight(this.cols[0]);
+		comp.textIn("受診科", this.cols[0], "center", "center");
+	}
+}
+
+class LowerBoxLayout {
+	frame: Box;
+
+	constructor(box: Box){
+		this.frame = box;
+	}
+
+	renderBlank(comp: Compiler, env: RenderEnv): void {
+		comp.box(this.frame);
+	}
+}
+
 export class MeisaiForm {
+	private comp: Compiler;
+	private pages: Op[][] = [];
+
+	constructor(visit: Visit | null, patient: Patient | null, meisai: VisitMeisai | null){
+		this.comp = new Compiler();
+		let env = new RenderEnv();
+		let layout = new Layout();
+		let comp = this.comp;
+		comp.createFont("title-font", "MS Gothic", 6);
+		comp.createFont("regular", "MS Gothic", 3);
+		comp.createPen("regular", 0, 0, 0, 0.4);
+		comp.setPen("regular");
+		env.titleFont = "title-font";
+		env.regularFont = "regular"
+		layout.renderBlank(comp, env);
+	}
+
+	done(): void {
+		let ops = this.comp.getOps();
+		if( ops.length > 0 ){
+			this.pages.push(ops);
+		}
+	}
+
+	getPages(): Op[][] {
+		return this.pages;
+	}
+}
+
+export class MeisaiFormOrig {
 	private comp: Compiler;
 	private pages: Op[][];
 	private visit: Visit | null;
 	private patient: Patient | null;
 	private meisai: VisitMeisai | null;
+	private outerFrame: Box;
 
 	constructor(visit: Visit | null, patient: Patient | null, meisai: VisitMeisai | null){
 		this.comp = new Compiler();
@@ -19,6 +154,8 @@ export class MeisaiForm {
 		this.visit = visit;
 		this.patient = patient;
 		this.meisai = meisai;
+		this.outerFrame = this.pageA4();
+		this.prelude();
 		let outer = this.pageA4();
 		let box = outer.innerBox(30, 42, 30 + 140, 42 + 10 + 4 + 185);
 		let [upperBox, _, lowerBox] = box.splitToRows(10, 14);
@@ -33,6 +170,9 @@ export class MeisaiForm {
 		comp.textAt("診療明細書", box.cx(), 30, "center", "center");
 		this.renderUpperBox(upperBox);
 		this.renderLowerBox(lowerBox);
+	}
+
+	prelude(): void {
 	}
 
 	done(): void {
--- static/preview-bundle.js
+++ static/preview-bundle.js
@@ -27144,13 +27144,113 @@
 	    }
 	    ;
 	}
+	class Point {
+	    constructor(x, y) {
+	        this.x = x;
+	        this.y = y;
+	    }
+	}
+	class RenderEnv {
+	}
+	class Layout {
+	    constructor() {
+	        let page = myclinic_drawer_1.Box.createA4Box();
+	        let box = page.innerBox(30, 42, 30 + 140, 42 + 10 + 4 + 185);
+	        let [upperBox, _, lowerBox] = box.splitToRows(10, 14);
+	        this.titleCenterPoint = new Point(box.cx(), 30);
+	        this.upperBox = new UpperBoxLayout(upperBox);
+	        this.lowerBox = new LowerBoxLayout(lowerBox);
+	    }
+	    renderBlank(comp, env) {
+	        comp.setFont(env.titleFont);
+	        comp.textAt("診療明細書", this.titleCenterPoint.x, this.titleCenterPoint.y, "center", "center");
+	        comp.setFont(env.regularFont);
+	        this.upperBox.renderBlank(comp, env);
+	        this.lowerBox.renderBlank(comp, env);
+	    }
+	}
+	class UpperBoxLayout {
+	    constructor(box) {
+	        this.frame = box;
+	        let [row1, row2] = box.splitToEvenRows(2);
+	        this.row1 = new UpperBoxRow1Layout(row1);
+	        this.row2 = new UpperBoxRow2Layout(row2);
+	    }
+	    renderBlank(comp, env) {
+	        comp.box(this.frame);
+	        comp.frameBottom(this.row1.frame);
+	        this.row1.renderBlank(comp, env);
+	        this.row2.renderBlank(comp, env);
+	    }
+	}
+	class UpperBoxRow1Layout {
+	    constructor(box) {
+	        this.frame = box;
+	        this.cols = box.splitToColumnsByWidths(17, 30, 10, 40, 16);
+	    }
+	    renderBlank(comp, env) {
+	        let cols = this.cols;
+	        for (let i = 0; i < cols.length - 1; i++) {
+	            comp.frameRight(cols[i]);
+	        }
+	        comp.textIn("患者番号", cols[0], "center", "center");
+	        comp.textIn("氏名", cols[2], "center", "center");
+	        comp.textIn("受診日", cols[4], "center", "center");
+	    }
+	}
+	class UpperBoxRow2Layout {
+	    constructor(box) {
+	        this.frame = box;
+	        this.cols = box.splitToColumnsByWidths(17);
+	    }
+	    renderBlank(comp, env) {
+	        comp.frameRight(this.cols[0]);
+	        comp.textIn("受診科", this.cols[0], "center", "center");
+	    }
+	}
+	class LowerBoxLayout {
+	    constructor(box) {
+	        this.frame = box;
+	    }
+	    renderBlank(comp, env) {
+	        comp.box(this.frame);
+	    }
+	}
 	class MeisaiForm {
+	    constructor(visit, patient, meisai) {
+	        this.pages = [];
+	        this.comp = new myclinic_drawer_1.Compiler();
+	        let env = new RenderEnv();
+	        let layout = new Layout();
+	        let comp = this.comp;
+	        comp.createFont("title-font", "MS Gothic", 6);
+	        comp.createFont("regular", "MS Gothic", 3);
+	        comp.createPen("regular", 0, 0, 0, 0.4);
+	        comp.setPen("regular");
+	        env.titleFont = "title-font";
+	        env.regularFont = "regular";
+	        layout.renderBlank(comp, env);
+	    }
+	    done() {
+	        let ops = this.comp.getOps();
+	        if (ops.length > 0) {
+	            this.pages.push(ops);
+	        }
+	    }
+	    getPages() {
+	        return this.pages;
+	    }
+	}
+	exports.MeisaiForm = MeisaiForm;
+	class MeisaiFormOrig {
 	    constructor(visit, patient, meisai) {
 	        this.comp = new myclinic_drawer_1.Compiler();
 	        this.pages = [];
 	        this.visit = visit;
 	        this.patient = patient;
 	        this.meisai = meisai;
+	        this.outerFrame = this.pageA4();
+	        this.prelude();
 	        let outer = this.pageA4();
 	        let box = outer.innerBox(30, 42, 30 + 140, 42 + 10 + 4 + 185);
 	        let [upperBox, _, lowerBox] = box.splitToRows(10, 14);
@@ -27165,6 +27265,8 @@
 	        comp.textAt("診療明細書", box.cx(), 30, "center", "center");
 	        this.renderUpperBox(upperBox);
 	        this.renderLowerBox(lowerBox);
+	    }
+	    prelude() {
 	    }
 	    done() {
 	        let ops = this.comp.getOps();
@@ -27306,7 +27408,7 @@
 	        }
 	    }
 	}
-	exports.MeisaiForm = MeisaiForm;
+	exports.MeisaiFormOrig = MeisaiFormOrig;
 
 
 /***/ }
